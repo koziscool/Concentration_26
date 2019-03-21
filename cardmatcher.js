60a07ee1fd7dc51d7cdb8cee3e42dfbe1804687d
John Kosmicke <kozjob@yahoo.com>
2019-03-20 17:12:12 -0700

    update

---
+++ cardmatcher.js
@@ -0,0 +1,34 @@
+
+
+matcherController = {
+  init: function( size ) {
+    
+  },
+    
+};
+
+matcherView = {
+
+  updateGameView: function(  ) {
+    
+  },
+};
+
+$(document).ready( function() {
+
+  $("#grid-size").focus();
+  $("#grid-size-button").click( function(e){
+    e.preventDefault();
+    var size = $("#grid-size").val();
+    if( Math.floor( size / 2) === size / 2 ) {
+      console.log('tetris');
+      matcherController.init(size);
+      matcherView.updateGameView();
+      $("#grid-size-form").hide();
+    } else {
+      $("#grid-size").val("Bad input, try again.")
+    }
+  });
+
+});
+  
